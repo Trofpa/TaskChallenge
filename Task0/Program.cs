6442aa5b545b62eddcb1330800a1e0f7a238f08f
Trofpa <yaseinokaminari@gmail.com>
2022-10-05 22:56:26 +0300

    Task0 in C# added.

---
+++ Task0/Program.cs
@@ -0,0 +1,35 @@
+﻿/* 
+Условие задачи:
+Вывести квадрат числа
+
+ Автор: Трофимов П.А.
+*/
+
+
+using static System.Math;
+using static System.Console;
+
+Clear();
+int a = InputNumbers("Введите число: ");
+WriteLine($"Квадрат числа: {Pow(a, 2)}");
+
+
+//Методы
+int InputNumbers(string text)
+{
+    bool Condition = true;
+    int number = 0;
+    while (Condition)
+    {
+        Write(text);
+        if (int.TryParse(ReadLine(), out number))
+        {
+            Condition = false;
+        }
+        else
+        {
+            WriteLine("Введенный символ не является числом!");
+        }
+    }
+return number;
+}
