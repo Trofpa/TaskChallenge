e1b78f19f50984ddc9fb419a08eb24a33518723f
Trofpa <yaseinokaminari@gmail.com>
2022-10-12 23:06:57 +0300

    Task7 and Task9 added

---
+++ Task7/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Показать числа от -N до N
+
+Автор: Трофимов П.А.
+*/
+
+
+//Методы:
+
+int[] NumbersArray(int number)
+{
+    int[] array = new int[2 * number + 1];
+    number *= -1;
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = number + i;
+    }
+    Console.WriteLine($"[" + String.Join(", ", array) + "]");
+    return array;
+}
+
+int[] arr = NumbersArray(10);
---
+++ Task9/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Показать последнюю цифру трёхзначного числа
+
+
+Автор: Трофимов П.А.
+
+*/
+
+//Методы
+
+int ThirdDigit(int number)
+{
+    int result;
+    int temp = 0;
+    if ((number >= 100 ) && (number < 1000))
+    {
+        result = number % 10;
+    }
+    else
+    {
+        Console.WriteLine("Не трехзначное число!");
+        result = -1;
+    }
+    return result;
+}
+
+int N = 465;
+Console.WriteLine(ThirdDigit(N));
